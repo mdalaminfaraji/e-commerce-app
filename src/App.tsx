2b1cfce082d2f81f465f358063b1ec8b88edc536
mdalaminfaraji <alaminice1617@gmail.com>
2025-04-28 18:17:27 +0600

    Refactor routing: centralize route config in routes.tsx, update App.tsx to use dynamic route rendering, and add product type definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConfigProvider } from 'antd';
-import { store } from './app/store';
-
-// Layouts
-import MainLayout from './layouts/MainLayout';
-
-// Pages
-import ProductList from './pages/ProductList';
-import ProductDetail from './pages/ProductDetail';
-import ProductEdit from './pages/ProductEdit';
-
-// Styles
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigProvider } from "antd";
+import { store } from "./store/store";
+import { appRoutes } from "./routes/routes";
+import { renderRoutes } from "./routes/routes";
+import "./App.css";
+const App = () => {
   return (
     <Provider store={store}>
       <ConfigProvider
         theme={{
           token: {
-            colorPrimary: '#1890ff',
+            colorPrimary: "#1890ff",
             borderRadius: 6,
           },
         }}
       >
         <Router>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<ProductList />} />
-              <Route path="product/:id" element={<ProductDetail />} />
-              <Route path="product/edit/:id" element={<ProductEdit />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Route>
-          </Routes>
+          <Routes>{renderRoutes(appRoutes)}</Routes>
         </Router>
       </ConfigProvider>
     </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,31 @@
+import MainLayout from "../layouts/MainLayout";
+import ProductList from "../pages/ProductList";
+import ProductDetail from "../pages/ProductDetail";
+import ProductEdit from "../pages/ProductEdit";
+import { Navigate, Route } from "react-router-dom";
+
+export interface AppRoute {
+  path: string;
+  element: React.ReactNode;
+  children?: AppRoute[];
+}
+
+export const appRoutes: AppRoute[] = [
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "", element: <ProductList /> },
+      { path: "product/:id", element: <ProductDetail /> },
+      { path: "product/edit/:id", element: <ProductEdit /> },
+      { path: "*", element: <Navigate to="/" replace /> },
+    ],
+  },
+];
+export function renderRoutes(routes: AppRoute[]) {
+  return routes.map(({ path, element, children }, idx) => (
+    <Route key={path + idx} path={path} element={element}>
+      {children && renderRoutes(children)}
+    </Route>
+  ));
+}
